64a9342b39d5ec800915abeee77cf964272d8e72
Wim Vandeweyer <wvandeweyer@gmail.com>
2026-03-13 19:49:36 +0100

    PWA

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'rekenheld-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-180.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+// Install: cache all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request)
+        .then(response => {
+          // Cache new successful requests
+          if (response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          }
+          return response;
+        })
+      )
+      .catch(() => caches.match('./index.html'))
+  );
+});
